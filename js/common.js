3388e5f6f3ddbaee0b788cd4c9f1ef065cafec65
ntrer <929727871@qq.com>
2020-04-28 15:17:58 +0800

    submit

--- js/common.js
+++ js/common.js
@@ -8,6 +8,8 @@ var API_ROOT = "http://47.114.181.11:48730"
 var live_val_Id = getQueryString("liveId");
 var token_val_id = getQueryString("token_id");
 var TAB_MESSAGE = getQueryString("tab");
+var latitude = "0";
+var longitude = "0";
 //分享人客户id
 // var liveShareCustomerId = getQueryString("liveShareCustomerId");
 //当前人客户id
@@ -108,6 +110,18 @@ function loadDefaultLiveShareSettings() {
 				cancel: function () {
 
 				}
+			});
+			
+			wx.getLocation({
+				type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+				success: function(res) {
+					latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+					longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+					//var speed = res.speed; // 速度，以米/每秒计
+					//var accuracy = res.accuracy; // 位置精度
+				},
+				fail: function(res) {},
+				cancel: function(res) {}
 			});
 		});
 	});
